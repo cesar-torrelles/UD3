29c0bb6dbe7aae7a62b2ff337b9e5da5598733e8
cesar-torrelles <raseksk8@gmail.com>
2021-02-03 11:16:46 +0100

    T4.4 ejercicio4

---
+++ T4.4/src/ejercicio4.java
@@ -0,0 +1,30 @@
+
+public class ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		// declaramos la variable entera N y le asignamos un valor
+		int N = 5;
+		
+		//imprimimos su valor
+		System.out.println("N vale: "+N);
+
+		//declaramos que N vale su valor inicial mas 77
+		N = N + 77;
+		
+		//N ahora vale 82, le restamos 3:
+		N = N - 3;
+		
+		//N ahora vale 79, lo duplicamos:
+		N = N * 2;
+		
+		System.out.println();
+		//imprimimos el valor resultante de todas las operaciones:
+		System.out.println("Despues de las operaciones N ahora vale: "+N);
+		
+		
+		
+				
+	}
+
+}
