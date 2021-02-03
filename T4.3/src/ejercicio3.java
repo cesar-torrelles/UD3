489086d7e9df47618fcfc98acf90bf0947d29e58
cesar-torrelles <raseksk8@gmail.com>
2021-02-03 11:10:48 +0100

    T4.3 ejercicio3

---
+++ T4.3/src/ejercicio3.java
@@ -0,0 +1,56 @@
+
+public class ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		//declaramos los valores de las variables:
+		int X = 3;
+		int Y = 4;
+		double N = 2.30;
+		double M = 3.45;
+		
+		//imprimimos los valores de cada variable:
+		System.out.println("Valor de cada variable:");
+		System.out.println("X = "+X);
+		System.out.println("Y = "+Y);
+		System.out.println("N = "+N);
+		System.out.println("M = "+M);
+		
+		//imprimimos el primer grupo de operaciones:
+		System.out.println();
+		System.out.println("X+Y = "+(X+Y));
+		System.out.println("X-Y = "+(X-Y));
+		System.out.println("X*Y = "+(X*Y));
+		System.out.println("X/Y = "+(X/Y));
+		System.out.println("X%Y = "+(X%Y));
+		
+		//imprimimos el segundo grupo de operaciones:
+		System.out.println();
+		System.out.println("N+M = "+(N+M));
+		System.out.println("N-M = "+(N-M));
+		System.out.println("N*M = "+(N*M));
+		System.out.println("N/M = "+(N/M));
+		System.out.println("N%M = "+(N%M));
+		
+		//imprimimos el tercer grupo:
+		System.out.println();
+		System.out.println("X+N = "+(X+N));
+		System.out.println("Y/M = "+(Y/M));
+		System.out.println("Y%M = "+(Y%M));
+		
+		//imprimimos el cuarto grupo:
+		System.out.println();	
+		System.out.println("X*2 = "+(X*2));
+		System.out.println("Y*2 = "+(Y*2));
+		System.out.println("M*2 = "+(M*2));
+		System.out.println("N*2 = "+(N*2));
+		
+		//imprimimos el quinto grupo:
+		System.out.println();
+		System.out.println("X+Y+N+M = "+(X+Y+N+M));
+		System.out.println("X*Y*N*M = "+(X*Y*N*M));
+
+		
+	}
+
+}
