94097c938f9cd9e7f1c1007999a0b80a30af72f0
cesar-torrelles <raseksk8@gmail.com>
2021-02-03 10:26:30 +0100

    T4.2 ejercicio2

---
+++ T4.2/src/ejercicio2.java
@@ -0,0 +1,43 @@
+
+public class ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		//declaramos las variables solicitadas:
+		
+		// N tipo int
+		int N = 5;
+		
+		// A tipo double
+		double A = 2.5 ;
+		
+		// C tipo char
+		char C = 'i';
+		
+		// imprimimos el valor de cada variable:
+		System.out.println("N = "+N);
+		System.out.println("A = "+A);
+		System.out.println("C = "+C);
+		
+		//imprimimos un espacio y la suma de N + A:
+		System.out.println();
+		System.out.println("N+A = "+(N+A));
+		
+		//imprimimos el resultado de A - N:
+		System.out.println("A-N = "+(A-N));
+		
+		//por ultimo, guardamos el valor de C en una nueva variable de tipo numerico (int)
+		int D = C;
+		
+		//dandonos como resultado el valor numerico del caracter que asignamos a C
+		System.out.println("Valor numerico del caracter: "+ C +" = " +D);
+
+
+
+		
+		
+				
+		
+	}
+
+}
