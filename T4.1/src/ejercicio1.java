399993e61856573c1eb9d8d04cdb9baad5b02894
cesar-torrelles <raseksk8@gmail.com>
2021-02-03 10:15:57 +0100

    T4.1 ejercicio1

---
+++ T4.1/src/ejercicio1.java
@@ -0,0 +1,31 @@
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		//declaramos dos variables numericas y las imprimimos:
+		int a =4;
+		System.out.println("variable a = "+a);
+		int b =6;
+		System.out.println("variable b = "+b);
+		
+		//añadimos un espacio:	
+		System.out.println();
+
+		//realizamos una suma y lo imprimimos:
+		System.out.println("a+b = "+(a+b));
+		
+		//realizamos una resta y lo imprimimos:
+		System.out.println("a-b = "+(a-b));
+		
+		//realizamos una multiplicacion y lo imprimimos:
+		System.out.println("a*b = "+(a*b));
+		
+		//realizamos una division y lo imprimimos:
+		System.out.println("a/b = "+(a/b));
+		
+		//realizamos un modulo y lo imprimimos:
+		System.out.println("a%b = "+(a%b));
+	}
+
+}
